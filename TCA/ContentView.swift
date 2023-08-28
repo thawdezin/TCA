ae9851c68c7ba9b4937a69afe2e4988765addf8b
Thaw De Zin <thawdezin@gmail.com>
2023-08-28 19:42:12 +0630

    Initial Commit

---
+++ TCA/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TCA
+//
+//  Created by Thaw De Zin on 8/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TCA/TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAApp.swift
+//  TCA
+//
+//  Created by Thaw De Zin on 8/28/23.
+//
+
+import SwiftUI
+
+@main
+struct TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
